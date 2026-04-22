471919fb94a673ab02f11dd1788c09f43e3523c0
Yundi339 <yundi339@foxmail.com>
2026-04-23 00:47:25 +0800

    feat(i18n): set default locale to zh-CN for improved localization support

--- apps/client/src/i18n.ts
+++ apps/client/src/i18n.ts
@@ -12,6 +12,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    lng: "zh-CN",
     fallbackLng: "zh-CN",
     debug: false,
     showSupportNotice: false,
